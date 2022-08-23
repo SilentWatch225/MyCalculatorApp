51dba585ad2cf658534c5b6f647939bd983dacf0
SilentWatch225 <russmike225@gmail.com>
2022-08-22 21:19:03 -0500

    Completed the first Activity in the App, work on the radical calculator is in progress 8/22/2022

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,21 +21,78 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         //Takes the second number entered and puts it into an editText
         EditText number2ET = findViewById(R.id.num2ET);
-        //Takes the symbol entered and puts it into an editText
-        EditText number3ET = findViewById(R.id.num3ET);
         //Takes the textView and sets it in the java
         TextView numberSumTV = findViewById(R.id.resultTV);
 
         //Modified the numbers to accept doubles
         double num1 = Double.parseDouble((number1ET.getText().toString()));
         double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double sum;
+        double sum = num1 + num2;
 
+        numberSumTV.setText("" + sum);
+    }
 
+    public void findSub(View view){
+        //Takes the first number entered and puts it into an editText
+        EditText number1ET = findViewById(R.id.num1ET);
+        //Takes the second number entered and puts it into an editText
+        EditText number2ET = findViewById(R.id.num2ET);
+        //Takes the textView and sets it in the java
+        TextView numberSumTV = findViewById(R.id.resultTV);
 
-        //Create try catch method or limit user input to only numbers
+        //Modified the numbers to accept doubles
+        double num1 = Double.parseDouble((number1ET.getText().toString()));
+        double num2 = Double.parseDouble((number2ET.getText().toString()));
+        double sub = num1 - num2;
 
-        //Uncomment this line when you finished the try catch
-        //numberSumTV.setText("" + sum);
+        numberSumTV.setText("" + sub);
+    }
+
+    public void findMult(View view){
+        //Takes the first number entered and puts it into an editText
+        EditText number1ET = findViewById(R.id.num1ET);
+        //Takes the second number entered and puts it into an editText
+        EditText number2ET = findViewById(R.id.num2ET);
+        //Takes the textView and sets it in the java
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        //Modified the numbers to accept doubles
+        double num1 = Double.parseDouble((number1ET.getText().toString()));
+        double num2 = Double.parseDouble((number2ET.getText().toString()));
+        double mult = num1 * num2;
+
+        numberSumTV.setText("" + mult);
+    }
+
+    public void findDiv(View view){
+        //Takes the first number entered and puts it into an editText
+        EditText number1ET = findViewById(R.id.num1ET);
+        //Takes the second number entered and puts it into an editText
+        EditText number2ET = findViewById(R.id.num2ET);
+        //Takes the textView and sets it in the java
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        //Modified the numbers to accept doubles
+        double num1 = Double.parseDouble((number1ET.getText().toString()));
+        double num2 = Double.parseDouble((number2ET.getText().toString()));
+        double div = num1 / num2;
+
+        numberSumTV.setText("" + div);
+    }
+
+    public void findMod(View view){
+        //Takes the first number entered and puts it into an editText
+        EditText number1ET = findViewById(R.id.num1ET);
+        //Takes the second number entered and puts it into an editText
+        EditText number2ET = findViewById(R.id.num2ET);
+        //Takes the textView and sets it in the java
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        //Modified the numbers to accept doubles
+        double num1 = Double.parseDouble((number1ET.getText().toString()));
+        double num2 = Double.parseDouble((number2ET.getText().toString()));
+        double mod = num1 % num2;
+
+        numberSumTV.setText("" + mod);
     }
 }
